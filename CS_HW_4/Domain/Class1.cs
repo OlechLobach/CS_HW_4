ce4e0e72393311273296aca1b785e5ff5d568221
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-03-17 00:34:54 +0200

    initiall commit

--- CS_HW_4/Domain/Class1.cs
+++ CS_HW_4/Domain/Class1.cs
@@ -1,1 +1,59 @@
-﻿
+﻿using System;
+using System.Collections.Generic; // Додано директиву
+
+namespace Domain
+{
+    public class Passport
+    {
+        public string FullName { get; set; }
+        public string PassportNumber { get; set; }
+        public string Nationality { get; set; }
+
+        public Passport(string fullName, string passportNumber, string nationality)
+        {
+            FullName = fullName;
+            PassportNumber = passportNumber;
+            Nationality = nationality;
+        }
+
+        public virtual void PrintInfo() 
+        {
+            Console.WriteLine($"Full Name: {FullName}");
+            Console.WriteLine($"Passport Number: {PassportNumber}");
+            Console.WriteLine($"Nationality: {Nationality}");
+        }
+    }
+
+    public class ForeignPassport : Passport
+    {
+        public List<string> Visas { get; set; }
+        public string ForeignPassportNumber { get; set; }
+
+        public ForeignPassport(string fullName, string passportNumber, string nationality, string foreignPassportNumber)
+            : base(fullName, passportNumber, nationality)
+        {
+            ForeignPassportNumber = foreignPassportNumber;
+            Visas = new List<string>();
+        }
+
+        public void AddVisa(string visa)
+        {
+            Visas.Add(visa);
+        }
+
+        public void PrintVisas()
+        {
+            Console.WriteLine("Visas:");
+            foreach (var visa in Visas)
+            {
+                Console.WriteLine(visa);
+            }
+        }
+
+        public override void PrintInfo()
+        {
+            base.PrintInfo();
+            Console.WriteLine($"Foreign Passport Number: {ForeignPassportNumber}");
+        }
+    }
+}
--- CS_HW_4/Program.cs
+++ CS_HW_4/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System;
+using Domain;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main()
+        {
+            Passport passport = new Passport("John Doe", "123456789", "USA");
+            passport.PrintInfo();
+
+            ForeignPassport foreignPassport = new ForeignPassport("John Doe", "123456789", "USA", "ABC123");
+            foreignPassport.AddVisa("Schengen");
+            foreignPassport.AddVisa("UK");
+            foreignPassport.PrintInfo();
+            foreignPassport.PrintVisas();
+        }
+    }
+}
