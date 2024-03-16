94c2e2d27652675e309043c4516e3d5a36051282
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-03-17 00:53:33 +0200

    initiall commit

--- CS_HW_4/Domain/Class1.cs
+++ CS_HW_4/Domain/Class1.cs
@@ -1,1 +1,73 @@
-﻿
+﻿using System;
+using ShapesBase;
+
+namespace ShapeTypes
+{
+    public class Rectangle : Shape
+    {
+        public double Width { get; }
+        public double Height { get; }
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override double CalculateArea()
+        {
+            return Width * Height;
+        }
+    }
+
+    public class Circle : Shape
+    {
+        public double Radius { get; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    public class Triangle : Shape
+    {
+        public double Base { get; }
+        public double Height { get; }
+
+        public Triangle(double @base, double height)
+        {
+            Base = @base;
+            Height = height;
+        }
+
+        public override double CalculateArea()
+        {
+            return 0.5 * Base * Height;
+        }
+    }
+
+    public class Trapezoid : Shape
+    {
+        public double TopBase { get; }
+        public double BottomBase { get; }
+        public double Height { get; }
+
+        public Trapezoid(double topBase, double bottomBase, double height)
+        {
+            TopBase = topBase;
+            BottomBase = bottomBase;
+            Height = height;
+        }
+
+        public override double CalculateArea()
+        {
+            return 0.5 * (TopBase + BottomBase) * Height;
+        }
+    }
+}
--- CS_HW_4/Program.cs
+++ CS_HW_4/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using System;
+using ShapesBase;
+using ShapeTypes;
+
+namespace ShapeApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Shape[] shapes = new Shape[]
+            {
+                new Rectangle(5, 10),
+                new Circle(7),
+                new Triangle(3, 4),
+                new Trapezoid(3, 5, 6)
+            };
+
+            foreach (var shape in shapes)
+            {
+                Console.WriteLine($"Area: {shape.CalculateArea()}");
+            }
+        }
+    }
+}
