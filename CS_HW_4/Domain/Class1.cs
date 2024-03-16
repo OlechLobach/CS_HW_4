86277ad4e36836583e87d6a6975ef53cc8a4080a
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-03-17 01:18:37 +0200

    initiall commit

--- CS_HW_4/Domain/Class1.cs
+++ CS_HW_4/Domain/Class1.cs
@@ -1,1 +1,69 @@
-﻿
+﻿using System;
+
+namespace DevicesLibrary
+{
+    public class Device
+    {
+        public string Name { get; }
+        public string Characteristics { get; }
+
+        public Device(string name, string characteristics)
+        {
+            Name = name;
+            Characteristics = characteristics;
+        }
+
+        public virtual void Sound()
+        {
+            Console.WriteLine("Device sound");
+        }
+    }
+
+    public class Kettle : Device
+    {
+        public Kettle(string name, string characteristics) : base(name, characteristics)
+        {
+        }
+
+        public override void Sound()
+        {
+            Console.WriteLine("Kettle sound");
+        }
+    }
+
+    public class Microwave : Device
+    {
+        public Microwave(string name, string characteristics) : base(name, characteristics)
+        {
+        }
+
+        public override void Sound()
+        {
+            Console.WriteLine("Microwave sound");
+        }
+    }
+
+    public class Car : Device
+    {
+        public Car(string name, string characteristics) : base(name, characteristics)
+        {
+        }
+
+        public override void Sound()
+        {
+            Console.WriteLine("Car sound");
+        }
+    }
+
+    public class Steamship : Device
+    {
+        public Steamship(string name, string characteristics) : base(name, characteristics)
+        {
+        }
+
+        public override void Sound()
+        {
+            Console.WriteLine("Steamship sound");
+        }
+    }
+}
--- CS_HW_4/Program.cs
+++ CS_HW_4/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using DevicesLibrary;
+
+namespace DeviceApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Device kettle = new Kettle("Electric kettle", "Capacity: 1.7L, Power: 1500W");
+            Device microwave = new Microwave("Microwave oven", "Capacity: 25L, Power: 1000W");
+            Device car = new Car("Electric car", "Range: 250 miles, Power: 150kW");
+            Device steamship = new Steamship("Steamship", "Speed: 25 knots, Capacity: 200 passengers");
+
+            PlaySound(kettle);
+            PlaySound(microwave);
+            PlaySound(car);
+            PlaySound(steamship);
+        }
+
+        static void PlaySound(Device device)
+        {
+            Console.Write($"{device.Name} characteristics: {device.Characteristics}. ");
+            device.Sound();
+        }
+    }
+}
