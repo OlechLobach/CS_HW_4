14ec5c8cc9e62365a65d6f5a4ed7051fe1a20088
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-03-17 00:23:54 +0200

    initiall commit

--- CS_HW_4/Domain/Class1.cs
+++ CS_HW_4/Domain/Class1.cs
@@ -1,1 +1,76 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Human
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Human(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Name: {Name}, Age: {Age}");
+        }
+    }
+
+    // Builder.cs
+    public class Builder : Human
+    {
+        public string Specialization { get; set; }
+
+        public Builder(string name, int age, string specialization)
+            : base(name, age)
+        {
+            Specialization = specialization;
+        }
+
+        public void Build()
+        {
+            Console.WriteLine($"{Name} is a builder specializing in {Specialization}");
+        }
+    }
+
+    // Sailor.cs
+    public class Sailor : Human
+    {
+        public string ShipType { get; set; }
+
+        public Sailor(string name, int age, string shipType)
+            : base(name, age)
+        {
+            ShipType = shipType;
+        }
+
+        public void Sail()
+        {
+            Console.WriteLine($"{Name} is a sailor on a {ShipType}");
+        }
+    }
+
+    // Pilot.cs
+    public class Pilot : Human
+    {
+        public string AircraftType { get; set; }
+
+        public Pilot(string name, int age, string aircraftType)
+            : base(name, age)
+        {
+            AircraftType = aircraftType;
+        }
+
+        public void Fly()
+        {
+            Console.WriteLine($"{Name} is a pilot of a {AircraftType}");
+        }
+    }
+}
--- CS_HW_4/Program.cs
+++ CS_HW_4/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using Domain;
+using System;
+
+namespace CS_HW_4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Builder builder = new Builder("John", 35, "residential buildings");
+            builder.PrintInfo();
+            builder.Build();
+
+            Sailor sailor = new Sailor("Mike", 42, "cargo ship");
+            sailor.PrintInfo();
+            sailor.Sail();
+
+            Pilot pilot = new Pilot("Alice", 30, "passenger airplane");
+            pilot.PrintInfo();
+            pilot.Fly();
+        }
+    }
+}   
